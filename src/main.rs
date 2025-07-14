d50c7a0739bc328343c7cf12bb482a48a5dd07da
Jmeow <j@jmeow.net>
2025-07-14 18:12:00 -0500

    Fix CAFEBABE detection so it doesn't say it's true if it only starts with CAFE

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let data: Vec<u8> = fs::read("./demojavaprogram/Addition.class")?;
-    assert_eq!(data[0..2], [0xCA, 0xFE]);
+    assert_eq!(data[0..4], [0xCA, 0xFE, 0xBA, 0xBE]);
     Ok(())
 }
